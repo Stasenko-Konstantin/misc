f7376ddd73875107c0109ee5681712e873fd1376
Stasenko-Konstantin <stasenko.kost@yandex.ru>
2022-11-24 23:49:26 +0500

    racket-pamper

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 a variety of minor programs
 
 - twister - tiny android application for basic automating the game of twister
+- racket-pamper - a little racket dabbling
---
+++ racket-pamper/ufo.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+;; somewhat from htdp
+
+(require 2htdp/image
+         2htdp/universe)
+
+(define ufo (overlay (rectangle 40 4 "solid" "blue")
+                     (circle 10 "solid" "green")))
+
+(define (compute-border scene-coord image-coord)
+  (let* ([border 10]
+         [edge (- scene-coord border)])
+    (cond
+      [(< image-coord border) border]
+      [(> image-coord edge) edge]
+      [#t image-coord])))
+
+(define (my-place-image image scene-width scene-height image-width image-height)
+  (place-image image
+               (compute-border scene-width image-width)
+               (compute-border scene-height image-height)
+               (empty-scene scene-width scene-height)))
+
+(define (distance time speed)
+  (* time speed))
+
+(animate (lambda (time)
+           (my-place-image ufo 300 200 150 (distance time 3))))
