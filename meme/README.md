bcf8a2b6e68bc33929631bfee37f9324caf96974
Stasenko-Konstantin <stasenko.kost@yandex.ru>
2023-01-03 23:02:10 +0500

    start meme

---
+++ meme/README.md
@@ -0,0 +1,6 @@
+# meme
+it should take a png and make a gif where the image is spinning
+
+```bash
+go run main.go p.png out.gif
+```
---
+++ meme/main.go
@@ -0,0 +1,90 @@
+// 135
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/color/palette"
+	"image/draw"
+	"image/gif"
+	_ "image/png"
+	"os"
+)
+
+var table [][]color.Color
+
+func readTable(img image.Image) {
+	bounds := img.Bounds()
+	for x := 0; x < bounds.Max.X; x++ {
+		table = append(table, nil)
+		for y := 0; y < bounds.Max.Y; y++ {
+			table[x] = append(table[x], img.At(x, y))
+		}
+	}
+}
+
+func contains[T comparable](s []T, e T) bool {
+	for _, c := range s {
+		if c == e {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	const (
+		nframes = 64
+		delay   = 8
+	)
+	args := os.Args
+	if len(args) < 2 {
+		panic("Need image and gif as argument: ./meme in.png out.gif")
+	}
+	path := args[1]
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	img, _, err := image.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+	anim := gif.GIF{LoopCount: nframes, BackgroundIndex: 0}
+	bounds := img.Bounds()
+	readTable(img)
+	fmt.Println("start rendering")
+	percent := (bounds.Max.X / 2) / 10
+	var edges []int
+	fmt.Print("[")
+	for i := 1; i <= bounds.Max.X/2; i++ {
+		edges = append(edges, i, bounds.Max.X-i)
+		palleted := image.NewPaletted(bounds, palette.Plan9)
+		draw.Draw(palleted, bounds, img, image.Point{}, draw.Src)
+		for x := 0; x < bounds.Max.X; x++ {
+			for y := 0; y < bounds.Max.Y; y++ {
+				if contains(edges, x) {
+					palleted.Set(x, y, color.White)
+				} else {
+					palleted.Set(x, y, table[x][y])
+				}
+			}
+		}
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, palleted)
+		if i%percent == 0 {
+			fmt.Print("|")
+		}
+	}
+	f, err = os.Create(args[2])
+	if err != nil {
+		panic(err)
+	}
+	err = gif.EncodeAll(f, &anim)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("]")
+}
